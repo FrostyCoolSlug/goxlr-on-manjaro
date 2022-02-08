789c7e9ae047678261dc3c7cf21b4a8da54b756c
Craig McLure <craig@mclure.net>
2022-02-08 15:59:52 +0000

    Added support for GoXLR Mini (check /proc/asound for presence)

--- prepare_jack.sh
+++ prepare_jack.sh
@@ -3,10 +3,25 @@
 # This script simply prepares and configures Jack for the GoXLR
 ###########
 
+DEVICE="hw:GoXLR"
+
+echo "Looking for GoXLR Device.."
+if [ -d /proc/asound/GoXLRMini ]; then
+        echo "Found GoXLR Mini"
+        DEVICE="hw:GoXLRMini"
+fi
+
+if [ -d /proc/asound/GoXLR ]; then
+        echo "Found GoXLR Full"
+        DEVICE="hw:GoXLR"
+fi
+
+
+
 jack_control eps realtime true
 jack_control ds alsa
 
-jack_control dps device hw:GoXLR
+jack_control dps device $DEVICE
 jack_control dps period 512
 jack_control dps rate 48000
 jack_control dps nperiods 3
